6a62cb13437abdfa8520877c6c9831269e525c17
Andreu <andreu.santaren@gmail.com>
2023-08-13 17:13:17 +0200

    New lexer and token set.

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,43 @@
 //! # IZE Language
 //!
-//! IZE is a programming language specialized in data pipelines and ETL processes, designed to accomplish the following tasks:
+//! IZE is a domain-specific programming language specialized in data pipelines and ETL processes, designed to accomplish the following data related tasks:
+//! - Convert
+//! - Aggregate
+//! - Filter
+//! - Route
+//! - Expand
+//! - Buffer
+//! - Pack
 //!
-//! - Define data models.
-//! - Convert from different data formats.
-//! - Apply data transformations.
-//! - Route data from multiple origins to multiple destinations.
-//! - Define data fetching and pushing policies.
+//! It transpiles into Rust and can run in multiple environments:
+//! - System service (deamon) for Unix-like systems and Windows.
+//! - On-cloud Lambda/Function for AWS, Azure and Google Cloud.
+//! - CLI tool for Unix-like systems and Windows.
 //!
-//! It can either transcompile to Rust or run an interpreter.
-//!
-//! IZE is a statically typed, async oriented, and functional-styled programming language with a minimalistic and expressive syntax.
+//! Is statically typed, async oriented, and declarative, with a minimalistic and expressive syntax, influenced by:
+//! - Protobuf
+//! - Rust
+//! - Serde
+//! - Go
+//! - TypeScript
+//! - Vector VRL
+//! - New Relic Flex
+//! - SQL
 
 #![no_std]
 
 #[macro_use]
 extern crate alloc;
 
-mod env;
+//mod env;
+
+mod common;
 
 /// First strage: Lexical analyzer.
 pub mod lexer;
 
-/// Second stage: Parser.
-pub mod parser;
+// /// Second stage: Parser.
+// pub mod parser;
 
-/// Third stage: Interpreter.
-pub mod eval;
+// /// Third stage: Interpreter.
+// pub mod eval;
--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,28 @@
-use ize::{
-    eval::Interpreter,
-    lexer::{Line, TokenData},
-    parser::{Ast, LineParser},
-};
+use ize::lexer::{Lexeme, Lexer};
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
-    process::exit,
+    str,
 };
 
 fn main() {
     let file = File::open("izeware/test.iz").expect("Error opening file");
-    let reader = BufReader::new(file);
-
-    let lines: Vec<Line> = reader
-        .lines()
-        .enumerate()
-        .map(|(line_num, line)| {
-            let line = line.expect("Error reading line of code from file");
-            match Line::scan_tokens(&line, line_num) {
-                Ok(line) => line,
-                Err(err) => {
-                    println!(
-                        "Lexer Error: \"{}\" at line {} offset {}",
-                        err.message,
-                        line_num + 1,
-                        err.position + 1
-                    );
-                    exit(1);
-                }
-            }
-        })
-        .collect();
-
-    println!("------- LEXER\n");
-
-    // Print tokens
-    for l in &lines {
-        for _ in 0..l.position.indentation {
-            print!(" ");
-        }
-        for t in &l.tokens {
-            if let TokenData::None = t.data {
-                print!("{} ", t.token_type);
-            } else {
-                print!("{}{} ", t.token_type, t.data);
-            }
-        }
-        println!("");
-    }
-    println!("");
-
-    println!("------- PARSER\n");
-
-    let statements = lines
-        .into_iter()
-        .map(|l| {
-            let line_num = l.position.line_num;
-            match LineParser::parse(l) {
-                Ok(stmt) => {
-                    println!("{}", stmt);
-                    (line_num, stmt)
-                }
-                Err(err) => {
-                    println!(
-                        "Parser Error: \"{}\" at line {} offset {}",
-                        err.message,
-                        line_num + 1,
-                        err.offset + 1
-                    );
-                    exit(2);
-                }
-            }
-        })
-        .collect();
-
-    let ast = Ast::new(statements);
-
-    println!("");
-
-    println!("------- INTERPRETER\n");
-
-    let mut intr = Interpreter::new(console_log);
-    for (line_num, stmt) in ast.statements.iter() {
-        if let Err(err) = intr.eval_stmt(stmt, *line_num) {
-            println!(
-                "Interpreter Error: \"{}\" at line {} offset {}",
-                err.message,
-                line_num + 1,
-                err.offset + 1
-            );
-            exit(3);
+    let mut reader = BufReader::new(file);
+    let mut buf = Vec::<u8>::new();
+    reader
+        .read_to_end(&mut buf)
+        .expect("Failed reading file into buffer");
+    let code = match str::from_utf8(&buf) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 string: {}", e),
+    };
+    let mut lexer = Lexer::new(code);
+    loop {
+        let token = lexer.scan_token().expect("Error scanning tokens");
+        match token.lexeme {
+            Lexeme::EOF => break,
+            Lexeme::Nothing => {}
+            _ => println!("{:?}", token),
         }
     }
-    println!("");
-}
-
-fn console_log(data: &str) {
-    println!("CONSOLE: {}", data);
 }
